d3df5e1ea88688b14e7405edf51e5613836a4359
HarrySilan <silan.harry@gmail.com>
2022-06-19 16:57:14 +0800

    add teleport through list in Custom Teleport

--- cheat-library/src/user/cheat/teleport/CustomTeleports.h
+++ cheat-library/src/user/cheat/teleport/CustomTeleports.h
@@ -4,6 +4,7 @@
 
 #include <cheat-base/cheat/Feature.h>
 #include <cheat-base/config/Config.h>
+#include <set>
 
 namespace cheat::feature
 {
@@ -11,10 +12,25 @@ namespace cheat::feature
     {
     public:
         config::Field<config::Toggle<Hotkey>> f_DebugMode;
+        config::Field<config::Toggle<Hotkey>> f_Enabled;
+        config::Field<Hotkey> f_Next;
+        config::Field<Hotkey> f_Previous;
         static CustomTeleports& GetInstance();
         const FeatureGUIInfo& GetGUIInfo() const override;
 		void DrawMain() override;
+
+        virtual bool NeedStatusDraw() const override;
+        void DrawStatus() override;
+
     private:
+        std::vector<std::pair<std::string, app::Vector3>> teleports;
+        std::set<unsigned int> checkedIndices;
+        bool selectedByClick = false;
+        int selectedIndex = -1;
+        std::string selectedIndexName;
         CustomTeleports();
+        void OnNextKeyPressed();
+        void OnPreviousKeyPressed();
+        void UpdateIndexName();
     };
 }
