7bad3b025fddcfb7a03f21df1644e7a50ea80bd7
Joaquin <67109235+Taiga74164@users.noreply.github.com>
2022-07-20 21:33:24 -0600

    code refactor

--- cheat-library/src/user/cheat/player/NoClip.cpp
+++ cheat-library/src/user/cheat/player/NoClip.cpp
@@ -7,13 +7,13 @@
 #include <cheat/game/util.h>
 #include <cheat-base/render/renderer.h>
 
-namespace cheat::feature 
+namespace cheat::feature
 {
 	static void HumanoidMoveFSM_LateTick_Hook(app::HumanoidMoveFSM* __this, float deltaTime, MethodInfo* method);
 	app::Vector3 zero;
 
-    NoClip::NoClip() : Feature(),
-        NF(f_Enabled,           "No clip",				"NoClip", false),
+	NoClip::NoClip() : Feature(),
+		NF(f_Enabled,			"No clip",				"NoClip", false),
 		NF(f_NoAnimation,		"No Animation",			"NoClip", true),
 		NF(f_UseCustomKeys,		"Use Custom Hotkeys",	"NoClip", false),
 		NF(f_ForwardKey,		"Forward HotKey",		"NoClip", Hotkey(ImGuiKey_W)),
@@ -23,32 +23,52 @@ namespace cheat::feature
 		NF(f_AscendKey,			"Ascend HotKey",		"NoClip", Hotkey(ImGuiKey_Space)),
 		NF(f_DescendKey,		"Descend HotKey",		"NoClip", Hotkey(ImGuiKey_ModShift)),
 		NF(f_AltSpeedKey,		"Alt Speed Hotkey",		"NoClip", Hotkey(ImGuiKey_ModCtrl)),
-        NF(f_Speed,             "Speed",                "NoClip", 5.5f),
-        NF(f_CameraRelative,    "Relative to camera",   "NoClip", true),
-		NF(f_VelocityMode,      "Velocity mode",        "NoClip", false),
-		NF(f_FreeflightMode,    "Freeflight mode",      "NoClip", false),
-		NF(f_AltSpeedEnabled,	"Alt speed enabled",    "NoClip", false),
-		NF(f_AltSpeed,			"Alt speed",            "NoClip", 1.0f)
+		NF(f_Speed,				"Speed",				"NoClip", 5.5f),
+		NF(f_CameraRelative,	"Relative to camera",	"NoClip", true),
+		NF(f_VelocityMode,		"Velocity mode",		"NoClip", false),
+		NF(f_FreeflightMode,	"Freeflight mode",		"NoClip", false),
+		NF(f_AltSpeedEnabled,	"Alt speed enabled",	"NoClip", false),
+		NF(f_AltSpeed,			"Alt speed",			"NoClip", 1.0f)
 
-    {
+	{
 		HookManager::install(app::MoleMole_HumanoidMoveFSM_LateTick, HumanoidMoveFSM_LateTick_Hook);
 
 		events::GameUpdateEvent += MY_METHOD_HANDLER(NoClip::OnGameUpdate);
 		events::MoveSyncEvent += MY_METHOD_HANDLER(NoClip::OnMoveSync);
-    }
+	}
 
-    const FeatureGUIInfo& NoClip::GetGUIInfo() const
-    {
-        static const FeatureGUIInfo info{ "No-Clip", "Player", true };
-        return info;
-    }
+	const FeatureGUIInfo& NoClip::GetGUIInfo() const
+	{
+		static const FeatureGUIInfo info{ "No-Clip", "Player", true };
+		return info;
+	}
 
-    void NoClip::DrawMain()
-    {
+	void NoClip::DrawMain()
+	{
 		ConfigWidget("Enabled", f_Enabled, "Enables no-clip (fast speed + no collision).\n" \
-            "To move, use WASD, Space (go up), and Shift (go down).");
+			"To move, use WASD, Space (go up), and Shift (go down), or customize your own keys.");
 
 		ConfigWidget("No Animation", f_NoAnimation, "Disables player animations.");
+
+		ConfigWidget("Speed", f_Speed, 0.1f, 2.0f, 100.0f,
+			"No-clip move speed.\n" \
+			"Not recommended setting above 5.0.");
+
+		ConfigWidget("Camera-relative movement", f_CameraRelative,
+			"Move relative to camera view instead of avatar view/direction.");
+
+		ConfigWidget("Alternate No-clip", f_AltSpeedEnabled,
+			"Allows usage of alternate speed when holding down LeftCtrl key.\n" \
+			"Useful if you want to temporarily go faster/slower than the no-clip speed setting.");
+
+		if (f_AltSpeedEnabled) {
+			ConfigWidget("Alt Speed", f_AltSpeed, 0.1f, 2.0f, 100.0f,
+				"Alternate no-clip move speed.\n" \
+				"Not recommended setting above 5.0.");
+
+			ConfigWidget("Velocity mode", f_VelocityMode, "Use velocity instead of position to move.");
+			ConfigWidget("Freeflight mode", f_FreeflightMode, "Don't remove collisions");
+		}
 
 		ConfigWidget("Use Custom Keys", f_UseCustomKeys, "Enable the Use of Custom HotKeys");
 
@@ -61,47 +81,27 @@ namespace cheat::feature
 			ConfigWidget("Descend HotKey", f_DescendKey, "Set Descend Key");
 			ConfigWidget("Alt Speed Key", f_AltSpeedKey, "Set AltSpeed HotKey");
 		}
+	}
 
-		ConfigWidget("Speed", f_Speed, 0.1f, 2.0f, 100.0f,
-			"No-clip move speed.\n" \
-			"Not recommended setting above 5.0.");
+	bool NoClip::NeedStatusDraw() const
+	{
+		return f_Enabled;
+	}
 
-		ConfigWidget("Camera-relative movement", f_CameraRelative,
-			"Move relative to camera view instead of avatar view/direction.");
-
-		ConfigWidget("Alternate No-clip", f_AltSpeedEnabled,
-			"Allows usage of alternate speed when holding down LeftCtrl key.\n" \
-			"Useful if you want to temporarily go faster/slower than the no-clip speed setting.");
-			
-		if (f_AltSpeedEnabled) {
-			ConfigWidget("Alt Speed", f_AltSpeed, 0.1f, 2.0f, 100.0f,
-				"Alternate no-clip move speed.\n" \
-				"Not recommended setting above 5.0.");
-
-		ConfigWidget("Velocity mode", f_VelocityMode,"Use velocity instead of position to move.");
-		ConfigWidget("Freeflight mode", f_FreeflightMode,"Don't remove collisions");
-		}
-    }
-
-    bool NoClip::NeedStatusDraw() const
-{
-        return f_Enabled;
-    }
-
-    void NoClip::DrawStatus() 
-    {
+	void NoClip::DrawStatus()
+	{
 		ImGui::Text("NoClip%s[%.01f%s%|%s]",
 			f_AltSpeedEnabled ? "+Alt " : " ",
 			f_Speed.value(),
 			f_AltSpeedEnabled ? fmt::format("|{:.1f}", f_AltSpeed.value()).c_str() : "",
 			f_CameraRelative ? "CR" : "PR");
-    }
+	}
 
-    NoClip& NoClip::GetInstance()
-    {
-        static NoClip instance;
-        return instance;
-    }
+	NoClip& NoClip::GetInstance()
+	{
+		static NoClip instance;
+		return instance;
+	}
 
 	// No clip update function.
 	// We just disabling collision detect and move avatar when no clip moving keys pressed.
@@ -110,7 +110,7 @@ namespace cheat::feature
 		static bool isApplied = false;
 
 		auto& manager = game::EntityManager::instance();
-		
+
 		if (!f_Enabled && isApplied)
 		{
 			auto avatarEntity = manager.avatar();
@@ -119,7 +119,7 @@ namespace cheat::feature
 				return;
 
 			app::Rigidbody_set_detectCollisions(rigidBody, true, nullptr);
-			
+
 			isApplied = false;
 		}
 
@@ -141,65 +141,37 @@ namespace cheat::feature
 			return;
 		if (!f_FreeflightMode)
 			app::Rigidbody_set_detectCollisions(rigidBody, false, nullptr);
-		
+
 		if (!f_VelocityMode)
-			app::Rigidbody_set_velocity(rigidBody, zero,nullptr);
+			app::Rigidbody_set_velocity(rigidBody, zero, nullptr);
 
 		auto cameraEntity = game::Entity(reinterpret_cast<app::BaseEntity*>(manager.mainCamera()));
 		auto relativeEntity = f_CameraRelative ? &cameraEntity : avatarEntity;
 
 		float speed = f_Speed.value();
-		if (f_AltSpeedEnabled) {
-			if (!f_UseCustomKeys && Hotkey(ImGuiKey_ModCtrl).IsPressed()) {
-				speed = f_AltSpeed.value();
-			}
-			if (f_UseCustomKeys && f_AltSpeedKey.value().IsPressed()) {
-				speed = f_AltSpeed.value();
-			}
-		}
+		if (f_AltSpeedEnabled ? f_UseCustomKeys ? f_AltSpeedKey.value().IsPressed() : Hotkey(ImGuiKey_ModCtrl).IsPressed() : NULL)
+			speed = f_AltSpeed.value();
 
 		app::Vector3 dir = {};
 
-		if (f_UseCustomKeys) {
-			if (f_ForwardKey.value().IsPressed())
-				dir = dir + relativeEntity->forward();
+		if (f_UseCustomKeys ? f_ForwardKey.value().IsPressed() : Hotkey(ImGuiKey_W).IsPressed())
+			dir = dir + relativeEntity->forward();
 
-			if (f_BackKey.value().IsPressed())
-				dir = dir + relativeEntity->back();
+		if (f_UseCustomKeys ? f_BackKey.value().IsPressed() : Hotkey(ImGuiKey_S).IsPressed())
+			dir = dir + relativeEntity->back();
 
-			if (f_RightKey.value().IsPressed())
-				dir = dir + relativeEntity->right();
+		if (f_UseCustomKeys ? f_RightKey.value().IsPressed() : Hotkey(ImGuiKey_D).IsPressed())
+			dir = dir + relativeEntity->right();
 
-			if (f_LeftKey.value().IsPressed())
-				dir = dir + relativeEntity->left();
+		if (f_UseCustomKeys ? f_LeftKey.value().IsPressed() : Hotkey(ImGuiKey_A).IsPressed())
+			dir = dir + relativeEntity->left();
 
-			if (f_AscendKey.value().IsPressed())
-				dir = dir + avatarEntity->up();
+		if (f_UseCustomKeys ? f_AscendKey.value().IsPressed() : Hotkey(ImGuiKey_Space).IsPressed())
+			dir = dir + avatarEntity->up();
 
-			if (f_DescendKey.value().IsPressed())
-				dir = dir + avatarEntity->down();
-		}
+		if (f_UseCustomKeys ? f_DescendKey.value().IsPressed() : Hotkey(ImGuiKey_ModShift).IsPressed())
+			dir = dir + avatarEntity->down();
 
-		if (!f_UseCustomKeys) {
-			if (Hotkey(ImGuiKey_W).IsPressed())
-				dir = dir + relativeEntity->forward();
-
-			if (Hotkey(ImGuiKey_S).IsPressed())
-				dir = dir + relativeEntity->back();
-
-			if (Hotkey(ImGuiKey_D).IsPressed())
-				dir = dir + relativeEntity->right();
-
-			if (Hotkey(ImGuiKey_A).IsPressed())
-				dir = dir + relativeEntity->left();
-
-			if (Hotkey(ImGuiKey_Space).IsPressed())
-				dir = dir + avatarEntity->up();
-
-			if (Hotkey(ImGuiKey_ModShift).IsPressed())
-				dir = dir + avatarEntity->down();
-		}
-		
 		app::Vector3 prevPos = avatarEntity->relativePosition();
 		if (IsVectorZero(prevPos))
 			return;
