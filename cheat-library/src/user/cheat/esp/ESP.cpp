8884fd2d2c83668ae4de2ff968d9ddebfef8b776
NctimeAza <102260794+NctimeAza@users.noreply.github.com>
2022-08-29 02:56:38 +0800

    Add "Aranara" for NPC filters

--- cheat-library/src/user/cheat/esp/ESP.cpp
+++ cheat-library/src/user/cheat/esp/ESP.cpp
@@ -263,6 +263,16 @@ namespace cheat::feature
 				}
 			}
 			name = name.substr(pos1 + 1, pos2 - pos1 - 1);
+		}	
+		else if (name.find("Aranara") != std::string::npos)
+		{
+		char AA[] = "Aranara";
+		name = AA;
+		}
+		else if (name.find("Kanban") != std::string::npos)
+		{
+		char AA[] = "Paimon";
+		name = AA;
 		}
 		else
 		{
@@ -778,7 +788,7 @@ namespace cheat::feature
 		ADD_FILTER_FIELD(plant, Pinecone);
 		ADD_FILTER_FIELD(plant, Qingxin);
 		ADD_FILTER_FIELD(plant, Radish);
-		ADD_FILTER_FIELD(plant, RukkhashavaMushroom);
+		ADD_FILTER_FIELD(plant, RukkhashavaMushrooms);
 		ADD_FILTER_FIELD(plant, SakuraBloom);
 		ADD_FILTER_FIELD(plant, SangoPearl);
 		ADD_FILTER_FIELD(plant, SeaGanoderma);
--- cheat-library/src/user/cheat/game/filters.cpp
+++ cheat-library/src/user/cheat/game/filters.cpp
@@ -101,7 +101,7 @@ namespace cheat::game::filters
 		SimpleFilter Boar = { EntityType__Enum_1::Monster, "Boar" };
 		SimpleFilter Fox = { EntityType__Enum_1::Monster, "Fox" };
 		SimpleFilter Squirrel = { EntityType__Enum_1::Monster, "Squirrel" };
-		SimpleFilter Npc = { EntityType__Enum_1::NPC, { "_Liyue", "_Mengde", "_Inazuma", "_Enkanomiya", "_Sumeru", "_Fontaine", "_Natlan", "_Snezhnaya", "_Coop", "_Quest", "_Animal", "_Guide", "_Homeworld", "_Avatar", "_Kanban", "_Monster"} };
+		SimpleFilter Npc = { EntityType__Enum_1::NPC, { "Liyue", "Mengde", "Inazuma", "Enkanomiya", "Sumeru", "Fontaine", "Aranara", "Natlan", "Snezhnaya", "Coop", "Quest", "Animal", "Guide", "Homeworld", "Avatar", "Kanban", "Monster"} };
 		SimpleFilter Crane = { EntityType__Enum_1::Monster, "Crane" };
 		SimpleFilter Falcon = { EntityType__Enum_1::Monster, "Falcon" };
 		SimpleFilter LucklightFly = { EntityType__Enum_1::EnvAnimal, "Boltbug_" };
