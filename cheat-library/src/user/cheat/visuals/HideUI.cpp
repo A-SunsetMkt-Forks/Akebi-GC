a9079e24523739870dcc32728f0cfd6af8715460
NctimeAza <102260794+NctimeAza@users.noreply.github.com>
2022-09-03 05:48:26 +0800

    fix HideUI bug

--- cheat-library/src/user/cheat/visuals/HideUI.cpp
+++ cheat-library/src/user/cheat/visuals/HideUI.cpp
@@ -22,7 +22,7 @@ namespace cheat::feature
 
     void HideUI::DrawMain()
     {
-        ConfigWidget(f_Enabled, "Hide in-game UI.");
+        ConfigWidget(f_Enabled, "Hide in-game UI.\nWhen you switch to another scene (e.g. into Domains), turn off and re-enable this feature");
     }
 
     bool HideUI::NeedStatusDraw() const
@@ -47,15 +47,17 @@ namespace cheat::feature
         {
             if (ui_camera == nullptr)
                 ui_camera = app::GameObject_Find(string_to_il2cppi("/UICamera"), nullptr);
-
-            
+  
             if (ui_camera)
                 app::GameObject_SetActive(ui_camera, false, nullptr);
         }
         else
         {
             if (ui_camera)
+            {                
                 app::GameObject_SetActive(ui_camera, true, nullptr);
+                ui_camera = nullptr;
+            }                  
         }
     }
 }
