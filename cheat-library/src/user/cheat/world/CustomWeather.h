d2daa11a27b2c744ca02c0d0a865a586217d451d
Joaquin <67109235+Taiga74164@users.noreply.github.com>
2022-09-08 01:12:45 -0600

    Fixed #537

--- cheat-library/src/user/cheat/world/CustomWeather.h
+++ cheat-library/src/user/cheat/world/CustomWeather.h
@@ -1,27 +1,36 @@
 #pragma once
 #include <cheat-base/cheat/Feature.h>
 #include <cheat-base/config/config.h>
-#include <cheat-base/thread-safe.h>
 
 namespace cheat::feature
 {
 	class CustomWeather : public Feature
 	{
 	public:
+
+		enum class WeatherType
+		{
+			ClearSky,
+			Cloudy,
+			Foggy,
+			Storm,
+			RainHeavy,
+			FountainRain,
+			SnowLight,
+			EastCoast,
+		};
+
 		config::Field<config::Toggle<Hotkey>> f_Enabled;
 		config::Field<config::Toggle<Hotkey>> f_Lightning;
+		config::Field<config::Enum<WeatherType>> f_WeatherType;
 
 		static CustomWeather& GetInstance();
 		const FeatureGUIInfo& GetGUIInfo() const override;
 		void DrawMain() override;
 		bool NeedStatusDraw() const override;
 		void DrawStatus() override;
+
 	private:
-		int current_weather;
-		std::string GetWeather();
-		SafeQueue<uint32_t> toBeUpdate;
-		SafeValue<int64_t> nextUpdate;
-		int f_DelayUpdate = 1;
 		void OnGameUpdate();
 		CustomWeather();
 	};
